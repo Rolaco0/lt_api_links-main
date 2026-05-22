133a5862411ee5a707aec928b300a196b3ca414c
Rolaco <143848516+Rolaco0@users.noreply.github.com>
2026-05-22 09:23:07 +0300

    Update github-latest.js

--- api/github-latest.js
+++ api/github-latest.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://api.github.com/repos/madoiscool/ltsteamplugin/releases/latest');
+  const response = await fetch('https://api.github.com/repos/Rolaco0/RolacoTools/releases/latest');
   const data = await response.json();
   res.status(200).json(data);
 }
